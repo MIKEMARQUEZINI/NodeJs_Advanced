36efe0f84711f832414a03c51e674513fa8ebcf2
MIKEMARQUEZINI <marquezini.mike@outlook.com>
2023-11-18 17:59:18 -0300

    feat: update POST the User CRUD in controller

--- src/modules/User/controller/user-controller.ts
+++ src/modules/User/controller/user-controller.ts
@@ -1,22 +1,25 @@
-import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
+import { z } from 'zod';
 
 class UserController {
-  public async create(req: Request, res: Response) {
-    const prisma = new PrismaClient();
+  public create(req: Request, res: Response) {
+    const { nome, email, password } = req.body;
 
-    await prisma.user.create({
-      data: {
-        email: 'teste@emailteste.com',
-        name: 'Mario Bros',
-      },
-    });
-
-    return res.json({ data: 'User created' });
-  }
-
-  public read(req: Request, res: Response) {
-    return res.json({ data: 'Surprise Mother F#@k' });
+    try {
+      const ZUserSchema = z.object({
+        name: z.string().optional(),
+        email: z.string().email({ message: 'Email inválid' }),
+        password: z
+          .string()
+          .min(6, { message: 'Password is mandatory' })
+          .max(255),
+      });
+      ZUserSchema.parse({ name: nome, email, password });
+    } catch (err: any) {
+      return res
+        .status(400)
+        .json({ message: 'Dados invalidos', error: err.errors });
+    }
   }
 }
 export const userController = new UserController();
--- src/modules/User/router.ts
+++ src/modules/User/router.ts
@@ -4,7 +4,6 @@ import { userController } from './controller/user-controller';
 const router = Router();
 const baseUrl = '/user';
 
-router.get(`${baseUrl}/read`, userController.read);
-router.post(`${baseUrl}/create`, userController.create);
+router.post(`${baseUrl}`, userController.create);
 
 export const userRouter = router;
